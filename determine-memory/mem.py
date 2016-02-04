2b303aeb909314db6cf50a4aafac2b48855c6914
Shaun Walbridge <shaun.walbridge@gmail.com>
2016-02-04 15:10:55 -0500

    Py 3 compat / PEP8 improvements

--- determine-memory/mem.py
+++ determine-memory/mem.py
@@ -1,8 +1,10 @@
 import ctypes
 
+
 def ram():
     kernel32 = ctypes.windll.kernel32
     c_ulong = ctypes.c_ulong
+
     class MEMORYSTATUS(ctypes.Structure):
         _fields_ = [
             ('dwLength', c_ulong),
@@ -14,13 +16,13 @@ def ram():
             ('dwTotalVirtual', c_ulong),
             ('dwAvailVirtual', c_ulong)
         ]
-        
-    memoryStatus = MEMORYSTATUS()
-    memoryStatus.dwLength = ctypes.sizeof(MEMORYSTATUS)
-    kernel32.GlobalMemoryStatus(ctypes.byref(memoryStatus))
-    return (memoryStatus.dwTotalPhys, memoryStatus.dwAvailPhys)
+
+    memory_status = MEMORYSTATUS()
+    memory_status.dwLength = ctypes.sizeof(MEMORYSTATUS)
+    kernel32.GlobalMemoryStatus(ctypes.byref(memory_status))
+    return (memory_status.dwTotalPhys, memory_status.dwAvailPhys)
+
 r = ram()
 (total, available) = r
-print "total memory installed: {}".format(total / (1024**2))
-print "available memory: {}".format(available / (1024**2))
-
+print("total memory installed: {}".format(total / (1024**2)))
+print("available memory: {}".format(available / (1024**2)))
